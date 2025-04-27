7ec6d2eb70afc09912df8831ca39a645e91cd46e
Andrew Rogerson <nosregor@users.noreply.github.com>
2025-04-28 01:05:59 +0200

    clean health end point

--- dist/routes/healthz.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-router.get('/', (req, res) => {
-    res.status(200).json({ status: 'OK', timestamp: new Date() });
-});
-exports.default = router;
-//# sourceMappingURL=healthz.js.map
--- dist/routes/users.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-router.get('/users', (req, res) => {
-    res.json([{ id: 1, name: 'Alice' }]);
-});
-exports.default = router;
-//# sourceMappingURL=users.js.map
